66250ad0dd83975a761ac8856121937145690ddf
Harshamasina <harshamasina1999@gmail.com>
2025-10-21 17:03:00 -0500

    Project Structure Routing Sign In Sign Up

---
+++ components/AuthForm.tsx
@@ -0,0 +1,81 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button"
+import {Form} from "@/components/ui/form"
+import Image from "next/image";
+import Link from "next/link";
+import { toast } from "sonner";
+import FormField from "./FormField";
+import { useRouter } from "next/navigation";
+
+const authFormSchema = (type: FormType) => {
+    return z.object({
+        name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+        email: z.string().email(),
+        password: z.string().min(7),
+    })
+}
+
+const AuthForm = ({ type }: {type: FormType}) => {
+    const router = useRouter();
+    const formSchema = authFormSchema(type);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>){
+        try{
+            if(type === 'sign-up'){
+                toast.success('Account Created Successfully. Please sign in!')
+                router.push('/sign-in');
+            } else {
+                toast.success('Sign In Successfully.')
+                router.push('/');
+            }
+        } catch (error) {
+            console.error(error);
+            toast.error(`There was an error: ${error}`)
+        }
+    }
+
+    const isSignIn = type === 'sign-in';
+
+    return (
+        <div className="card-border lg:min-w-[566px]">
+            <div className="flex flex-col gap-6 card py-16 px-10">
+                <div className="flex flex-row gap-2 justify-center">
+                    <Image src="/logo.svg" alt="logo" height={32} width={38} />
+                    <h2 className="text-primary-200">TalkFlow AI</h2>
+                </div>
+                <h3 className="justify-center ml-10">Job Interview Preparation Platform</h3>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+                        { !isSignIn && (
+                            <FormField control={form.control} name="name" label="Enter Your Name" placeholder="Your Name" />
+                        )}
+                        <FormField control={form.control} name="email" type="email" label="Enter Your Email" placeholder="Your Email" />
+                        <FormField control={form.control} name="password" type="password" label="Enter Your Password" placeholder="Your Password" />
+                        <Button className="btn" type="submit">{ isSignIn ? 'Sign In' : "Create an Account" }</Button>
+                    </form>
+                </Form>
+
+                <p className="text-center">
+                    {isSignIn ? 'No Account Yet' : 'Have an account already?'}
+                    <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className="font-bold text-user-primary ml-1">
+                        {!isSignIn ? "Sign In" : "Sign Up"}
+                    </Link>
+                </p>
+            </div>
+        </div>
+    )
+};
+
+export default AuthForm;
