f1ec6261b13224013a915ab258bc2c101919bf02
Harshamasina <harshamasina1999@gmail.com>
2025-10-22 16:42:21 -0500

    added: "Firebase Authentication"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import robot from "../../public/robot.png";
+import { dummyInterviews } from "@/constants";
+import InterviewCard from "@/components/InterviewCard";
 
 const page = () => {
     return (
-        <div>page</div>
+        <>
+            <section className='card-cta'>
+                <div className='flex flex-col gap-6 max-w-lg'>
+                    <h2>Get Interview Ready with AI powered Practice and Feedback Chat Agent</h2>
+                    <p className='text-lg'>Practice on real Interview Questions</p>
+                    <Button asChild className="btn-primary max-sw:w-full">
+                        <Link href="/interview">Start an Interview</Link>
+                    </Button>
+                </div>
+
+                <Image src={robot} alt="robot AI" width={400} height={400} className="max-sm:hidden"/>
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interview Sessions</h2>
+                <div className="interviews-section">
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} key={interview.id} />
+                    ))}
+                    {/* <p>You haven't taken any interviews yet!</p> */}
+                </div>
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Take an Interview</h2>
+                <div className="interviews-section">
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} key={interview.id} />
+                    ))}
+                </div>
+            </section>
+        </>
     )
 };
 
