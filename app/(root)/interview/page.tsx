cd73d400ea671e35deb3b53e502e0a943f7f8f12
Harshamasina <harshamasina1999@gmail.com>
2025-10-23 12:03:18 -0500

    added: "Agent Layout

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,19 @@
+import Agent from "@/components/Agent";
+import { getCurrentuser } from "@/lib/actions/auth.action";
+
+const page = async () => {
+    const user  = await getCurrentuser();
+    return (
+        <>
+            <h3>Interview Generation</h3>
+            <Agent
+                userName={user?.name!}
+                userId={user?.id}
+                // profileImage={user?.profileURL}
+                type="generate"
+            />
+        </>
+    )
+};
+
+export default page;
